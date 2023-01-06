de7e9dc8dddbf249fef4d6f0d69285a4f5650dca
radarsymphony <grey8knights@gmail.com>
2023-01-06 13:18:06 -0800

    add nvim config

---
+++ .config/nvim/after/plugin/colors.lua
@@ -0,0 +1,5 @@
+-- Pywal color theme integration
+local pywal = require('pywal')
+pywal.setup()
+local pywal_core = require('pywal.core')
+local colors = pywal_core.get_colors()
---
+++ .config/nvim/init.lua
@@ -0,0 +1,4 @@
+-- neovim configuration initially copied from ThePrimeagen https://youtu.be/w7i4amO_zaE
+
+require("main")
+  
