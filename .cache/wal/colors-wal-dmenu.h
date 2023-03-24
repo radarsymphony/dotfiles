b335a0412dd9ff6a7f40f3fe53480f966a2d14dd
radarsymphony <grey8knights@gmail.com>
2023-03-24 11:40:45 -0700

    stop tracking wal cache

--- .cache/wal/colors-wal-dmenu.h
+++
@@ -1,6 +0,0 @@
-static const char *colors[SchemeLast][2] = {
-	/*     fg         bg       */
-	[SchemeNorm] = { "#c8c1c0", "#240a05" },
-	[SchemeSel] = { "#c8c1c0", "#b6371c" },
-	[SchemeOut] = { "#c8c1c0", "#b44d87" },
-};
--- .cache/wal/colors-wal-tabbed.h
+++
@@ -1,6 +0,0 @@
-static const char* selbgcolor   = "#240a05";
-static const char* selfgcolor   = "#c8c1c0";
-static const char* normbgcolor  = "#c59506";
-static const char* normfgcolor  = "#c8c1c0";
-static const char* urgbgcolor   = "#b6371c";
-static const char* urgfgcolor   = "#c8c1c0";
